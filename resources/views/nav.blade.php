bd5130311b42361a00df4757ed89eb42bb943887
Masashi Kawakami <m_kawakami@avelio.co.jp>
2022-02-28 01:16:59 +0900

    feature/login/signin/logout

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,32 +1,45 @@
 <nav class="navbar navbar-expand navbar-dark blue-gradient">
   <a class="navbar-brand" href="/"><i class="far fa-sticky-note mr-1"></i>memo</a>
   <ul class="navbar-nav ml-auto">
-    <li class="nav-item">
-      <a class="nav-link" href="">ユーザー登録</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="">ログイン</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
-    </li>
+    @guest
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('register') }}">ユーザー登録</a>
+      </li>
+    @endguest
+
+    @guest
+      <li class="nav-item">
+        <a class="nav-link" href="{{ route('login') }}">ログイン</a>
+      </li>
+    @endguest
+
+    @auth
+      <li class="nav-item">
+        <a class="nav-link" href=""><i class="fas fa-pen mr-1"></i>投稿する</a>
+      </li>
+    @endauth
+
     <!-- Dropdown -->
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="fas fa-user-circle"></i>
-      </a>
-      <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
-        <button class="dropdown-item" type="button" onclick="location.href=''">
-          マイページ
-        </button>
-        <div class="dropdown-divider"></div>
-        <button form="logout-button" class="dropdown-item" type="submit">
-          ログアウト
-        </button>
-      </div>
-    </li>
-    <form id="logout-button" method="POST" action="">
-    </form>
+    @auth
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <i class="fas fa-user-circle"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+          <button class="dropdown-item" type="button" onclick="location.href=''">
+            マイページ
+          </button>
+          <div class="dropdown-divider"></div>
+          <button form="logout-button" class="dropdown-item" type="submit">
+            ログアウト
+          </button>
+        </div>
+      </li>
+      <form id="logout-button" method="POST" action="{{ route('logout') }}">
+        @csrf
+      </form>
+    @endauth
     <!-- Dropdown -->
+
   </ul>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,5 @@
 //     return view('welcome');
 // });
 
+Auth::routes();
 Route::get('/', 'ArticleController@index');
